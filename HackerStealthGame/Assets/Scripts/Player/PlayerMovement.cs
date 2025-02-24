c7c93a2ce14675da14cffc0793fb687b06b55143
jgarcia18201 <jgarcia18201@gmail.com>
2025-02-23 18:11:08 -0600

    Included a blend tree with idle and move navigations

--- HackerStealthGame/Assets/Scripts/Player/PlayerMovement.cs
+++ HackerStealthGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     private CharacterController controller;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,5 +22,8 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = (transform.right * moveHorizontal + transform.forward * moveVertical) * speed;
         controller.Move(move * Time.deltaTime);
+
+        animator.SetFloat("Horizontal", moveHorizontal);
+        animator.SetFloat("Vertical", moveVertical);
     }
 }
