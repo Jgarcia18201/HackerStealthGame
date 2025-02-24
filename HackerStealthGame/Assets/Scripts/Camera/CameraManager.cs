de8828a218d42c2f71428364643be32b5fec1c52
jgarcia18201 <jgarcia18201@gmail.com>
2025-02-23 22:28:38 -0600

    added a script to MainCamera Gameobject for follow

---
+++ HackerStealthGame/Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private Vector3 _offset;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + _offset;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
